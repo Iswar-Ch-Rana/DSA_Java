08f31cf257b198ca8885a9f01cdac4f70c8703d7
Iswar Chandra Rana <ranabitu227@gmail.com>
2025-09-21 00:00:11 +0530

    added bruteforce solutions

--- src/a_5_FAQ_Medium/RearrangeArray.java
+++ src/a_5_FAQ_Medium/RearrangeArray.java
@@ -2,6 +2,7 @@ package a_5_FAQ_Medium;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class RearrangeArray {
     public static void main(String[] args) {
@@ -10,6 +11,7 @@ public class RearrangeArray {
         System.out.println("Output = " + Arrays.toString(ans));
     }
 
+    /** Solution - 2 TIME - O(n) Space - O(n) */
     public static int[] rearrangeArray(int[] nums) {
         int n = nums.length;
         int[] ans = new int[n];
@@ -25,5 +27,29 @@ public class RearrangeArray {
             }
         }
         return ans;
+    }
+
+    public int[] rearrangeArray1(int[] nums) {
+        int n = nums.length;
+
+        /* Define 2 vectors, one for storing positive
+        and other for negative elements of the array.*/
+        List<Integer> pos = new ArrayList<>();
+        List<Integer> neg = new ArrayList<>();
+
+        // Segregate the array into positives and negatives.
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) pos.add(nums[i]);
+            else neg.add(nums[i]);
+        }
+
+        // Positives on even indices, negatives on odd.
+        for (int i = 0; i < n / 2; i++) {
+            nums[2 * i] = pos.get(i);
+            nums[2 * i + 1] = neg.get(i);
+        }
+
+        // Return the result
+        return nums;
     }
 }
