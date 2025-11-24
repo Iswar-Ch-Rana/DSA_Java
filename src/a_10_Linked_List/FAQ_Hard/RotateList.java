c7ba6abdfd89a5b5c072761d14f568a9416e30f0
Iswar Chandra Rana <ranabitu227@gmail.com>
2025-11-24 21:48:56 +0530

    Rotate List

---
+++ src/a_10_Linked_List/FAQ_Hard/RotateList.java
@@ -0,0 +1,51 @@
+package a_10_Linked_List.FAQ_Hard;
+
+import a_10_Linked_List.CustomLinkedList.ListNode;
+
+public class RotateList {
+    public static void main(String[] args) {
+        ListNode head = ListNode.arrayToLinkedList(new int[]{1, 2, 3, 4, 5, 6, 7, 8});
+        head = rotateRight(head, 9);
+        ListNode.printLinkedList(head);
+    }
+
+    public static ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) return head;
+
+        // Step 1: Find length
+        int size = lengthOfLinkedList(head);
+        k = k % size;  // Optimize k
+        if (k == 0) return head;
+
+        // Step 2: Rotate at position size - k
+        return rotateByK(head, size - k);
+    }
+
+    private static ListNode rotateByK(ListNode head, int k) {
+        ListNode splitNode = head;
+        for (int i = 1; i < k; i++) {
+            splitNode = splitNode.next;
+        }
+
+        ListNode newHead = splitNode.next;
+        splitNode.next = null;  // Break
+
+        // Step 3: Reach new tail and connect
+        ListNode curr = newHead;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = head;
+
+        return newHead;
+    }
+
+    private static int lengthOfLinkedList(ListNode head) {
+        int count = 0;
+        while (head != null) {
+            count++;
+            head = head.next;
+        }
+        return count;
+    }
+}
