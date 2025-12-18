281b0ac53391f3a70f5d380d5addb0a68345588c
Iswar Chandra Rana <ranabitu227@gmail.com>
2025-12-18 21:04:46 +0530

    fomatting

--- src/a_14_Stack_And_Queue/Monotonic_Stack/RemoveKDigits.java
+++ src/a_14_Stack_And_Queue/Monotonic_Stack/RemoveKDigits.java
@@ -11,14 +11,14 @@ public class RemoveKDigits {
 
     public static String removeKdigits(String num, int k) {
         int len = num.length();
-        //corner case
+        // corner case
         if (k == len)
             return "0";
 
         Stack<Character> stack = new Stack<>();
         int i = 0;
         while (i < num.length()) {
-            //whenever meet a digit which is less than the previous digit, discard the previous one
+            // whenever meet a digit which is less than the previous digit, discard the previous one
             while (k > 0 && !stack.isEmpty() && stack.peek() > num.charAt(i)) {
                 stack.pop();
                 k--;
